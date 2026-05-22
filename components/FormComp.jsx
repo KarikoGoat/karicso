e03dfdfd9ffecc8936aa332de422cabec7ced0bc
KarikoGoat <114610654+KarikoGoat@users.noreply.github.com>
2026-05-22 12:56:46 +0200

    Update FormComp.jsx

--- components/FormComp.jsx
+++ components/FormComp.jsx
@@ -28,7 +28,7 @@ const MyTextArea = ({ label, ...props }) => {
 
 const FormComp = (props) => {
 
-  emailjs.init("G7UMGkHPcHIwayFoJ");
+  emailjs.init("dgRa1q9fTtk6kdqJt");
   const service_id = "service_r8om8id";
   const template_id = "template_qkvn6gc";
 
